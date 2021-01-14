7d20b7e17cc1c3ad182cf38b4d9f6548e6258aa8
Janina <zhaoyajanina@gamil.com>
2021-01-14 22:25:04 +0100

    vary the limit to test if can find steady state

--- ACDCACLineU.jl
+++ ACDCACLineU.jl
@@ -5,6 +5,8 @@ using PowerDynamics: PiModelLine
 using NetworkDynamics#: ODEEdge
 #using OrdinaryDiffEq: ODEFunction
 using DifferentialEquations
+# using GraphPlot
+
 dir = @__DIR__
 # include("$dir/LineMacro.jl")
 include("$dir/CIGRE_static_2ADN.jl")
@@ -32,6 +34,9 @@ verbose = false
 C_dc = ω * c * transmission_length / 4
 C_conv = ω * 20E-6
 Z_dcconv = 2 * r * transmission_length + 1im * (ω * l * transmission_length * 2 - 1 / (C_dc + C_conv))
+Y_dcconv = 1 / Z_dcconv
+k = 0.5  # from paper"An Equivalent Model for simulating VSC Based HVDC"
+M = (1/(k^2))
 
 #after each step is checked, from line 34 to 107 will be included in a function
 #function Grid_ACDC(P_ref1,P_ref2,Q_ref1,Q_ref2)
@@ -39,18 +44,27 @@ Z_dcconv = 2 * r * transmission_length + 1im * (ω * l * transmission_length * 2
     busses_static1, lines1, T1, elist1, Zs1, Yshs1 = CIGRE_static_ADN1()#1-12
     busses_static2, lines2, T2, elist2, Zs2, Yshs2 = CIGRE_static_ADN2()#13-24
     
-    k = 0.5  # from paper"An Equivalent Model for simulating VSC Based HVDC"
-    DCline = [StaticLine(;from=1, to=13, Y=1 / Z_dcconv *(1/(k^2)))]
+    
+    DCline = [StaticLine(;from=1, to=13, Y=Y_dcconv*M/base_admittance_HV)]
 
-    busses_static = append!(busses_static1, busses_static2)#1-24
-    lines = append!(lines1, lines2, DCline)
+    busses_static = []
+    append!(busses_static, busses_static1)#1-24
+    append!(busses_static, busses_static2)
+    lines = []
+    append!(lines, lines1)
+    append!(lines, lines2)
+    append!(lines, DCline)
 
     pg_static = PowerGrid(busses_static, lines) #consider if DC line also included head
     power_flow = pf_sol(pg_static, initial_guess(pg_static), nothing)
 
     busses = copy(busses_static)#1-24
 
-    DG_locs = 1:24 # located, 1 for slack, so 2 for MV1
+    DG_locs1 = collect(2:12) 
+    DG_locs2 =  collect(14:24)
+    DG_locs = []
+    append!(DG_locs, DG_locs1) # located, 1 for slack, so 2 for MV1
+    append!(DG_locs, DG_locs2)
     for i in DG_locs # this is a loop 
 
         S_bkgrnd = zero(im)
@@ -79,9 +93,9 @@ Z_dcconv = 2 * r * transmission_length + 1im * (ω * l * transmission_length * 2
 
         I_max=1.0, # pu
 
-        P_limit=1.0, # pu
+        P_limit=3.0, # pu
 
-        Q_limit=1.0, # pu
+        Q_limit=3.0, # pu
 
         Vref=1.0, # pu
 
@@ -93,27 +107,59 @@ Z_dcconv = 2 * r * transmission_length + 1im * (ω * l * transmission_length * 2
 
     )
 
+
     end
+#     S_bkgrnd = zero(im)
+#     busses[end] = DGUnit(;
+
+#     K_pll=1632.993, # Hz/pu
+
+#     T_int=2.0,
+
+#     K_PT1=1.0,
+
+#     T_PT1=1e-8,
+
+#     K_FRT=2.0,
+
+#     I_max=1.0, # pu
+
+#     P_limit=1000000.0, # pu
+
+#     Q_limit=1000000.0, # pu
+
+#     Vref=1.0, # pu
+
+#     Vdead=0.1, # pu
+
+#     S_pq=V -> S_bkgrnd * (quad * V^2 + 1 - quad), # quad can be chosen 1 or 0, voltage dependent or not
+
+#     Y_n=0.0,
+
+# )
    
     pg = PowerGrid(busses, lines)
 
-    # P_ref = 1
-    # Q_ref = -1
+    P_ref = 28
+    Q_ref = 0
     cpg = ADN(pg, DGUnit, t -> P_ref, t -> Q_ref) 
     #cpg = ADN(pg, DGUnit,  P_ref(t), Q_ref(t))
-    icguess_pf = initial_guess(cpg, power_flow[:, :u])
-
-    op = find_steady_state(cpg, icguess_pf) 
+    fp = initial_guess(cpg, power_flow[:, :u])
+    #power_flow_modified = power_flow
+    #power_flow_modified[:, :u][end] = power_flow[:,:u][12]
+    #fp_mod = initial_guess(cpg, power_flow_modified[:, :u])
+    
+    op = find_steady_state(cpg, fp) 
     # initial_guess(pg)  return State(pg, sol.u)
     verbose ? check_operationpoint(cpg, op) : nothing 
 
-    return pg, cpg
+    # return pg, cpg
 #end
 
 #pg = Grid_ACDC(P_ref1,P_ref2,Q_ref1,Q_ref2)
 
 #fp = find_valid_initial_condition(pg, ones(163))
-# fp = initial_guess(pg)
+#fp = initial_guess(cpg)
 tspan = (0, 20.0)
 sol = solve(pg, fp, tspan)
 #sol = solve(cpg, op, tspan)
